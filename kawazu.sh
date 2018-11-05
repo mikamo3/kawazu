9297477a839b4237f112c87c0b915548023192a5
mikamo3 <kamo3proj@gmail.com>
2018-11-06 01:19:27 +0900

    add console

---
+++ kawazu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+kawazu() {
+  if [[ $1 == "cd" ]]; then
+    cd "${KAWAZU_DOTFILES_REPOS_DIR:-$HOME/.kawazu/dotfiles}" || return 1
+  else
+    "${KAWAZU_ROOT_DIR:-$HOME/.kawazu/repos}/bin/kawazu" "$@"
+  fi
+}
