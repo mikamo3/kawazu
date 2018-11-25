dad872f055b1322cb593d2aea4d8bf0ab5d44c86
mikamo3 <kamo3proj@gmail.com>
2018-11-26 01:28:25 +0900

    refactor(console): fix escape and error and debug output to stderr

--- lib/console.sh
+++ lib/console.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #console output function
-readonly CL_BLACK="\E[30m"
-readonly CL_RED="\E[31m"
-readonly CL_GREEN="\E[32m"
-readonly CL_YELLOW="\E[33m"
-readonly CL_BLUE="\E[34m"
-readonly CL_PURPLE="\E[35m"
-readonly CL_CYAN="\E[36m"
-readonly CL_WHITE="\E[37m"
-readonly CL_RST="\E[0m"
+readonly CL_BLACK="\\E[30m"
+readonly CL_RED="\\E[31m"
+readonly CL_GREEN="\\E[32m"
+readonly CL_YELLOW="\\E[33m"
+readonly CL_BLUE="\\E[34m"
+readonly CL_PURPLE="\\E[35m"
+readonly CL_CYAN="\\E[36m"
+readonly CL_WHITE="\\E[37m"
+readonly CL_RST="\\E[0m"
 
 print_error() {
-  echo -e "${CL_RED}[✗] $1${CL_RST}"
+  echo -e "${CL_RED}[✗] $1${CL_RST} " 1>&2
 }
 print_success() {
   echo -e "${CL_GREEN}[✓] $1${CL_RST}"
@@ -20,7 +20,7 @@ print_info() {
   echo -e "${CL_CYAN}[i] $1${CL_RST}"
 }
 print_debug() {
-  [[ $OPT_DEBUG == "true" ]] && echo -e "${CL_PURPLE}[debug] $1${CL_RST}"
+  [[ $OPT_DEBUG == "true" ]] && echo -e "${CL_PURPLE}[debug] $1${CL_RST}" 1>&2
 }
 print_version() {
   echo "kawazu version $KAWAZU_VERSION"
