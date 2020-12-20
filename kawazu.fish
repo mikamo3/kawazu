2c1070f1ffb28f3b7bc82aab98388ab4906d2a9d
mikamo3 <kamo3proj@gmail.com>
2020-12-21 02:21:35 +0900

    fix cd command

--- kawazu.fish
+++ kawazu.fish
@@ -1,13 +1,19 @@
 #!/usr/bin/env fish
 function kawazu
   set kawazu_root_dir
+  set kawazu_dotfiles_dir
   if [ "{$KAWAZU_ROOT_DIR}" = "" ]
     set kawazu_root_dir {$HOME}/.kawazu/repos
     else
     set kawazu_root_dir {$KAWAZU_ROOT_DIR}
   end
+  if [ "{$KAWAZU_DOTFILES_DIR}" = "" ]
+    set kawazu_dotfiles_dir {$HOME}/.kawazu/dotfiles
+    else
+    set kawazu_dotfiles_dir {$KAWAZU_DOTFILES_DIR}
+  end
   if [ "{$argv}" = "cd" ]
-    {$kawazu_root_dir}/bin/kawazu cd
+    cd {$kawazu_dotfiles_dir}
   else
     {$kawazu_root_dir}/bin/kawazu $argv
   end
