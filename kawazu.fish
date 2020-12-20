5fe03c2469bc80f8e0fbb42c271bd8bab4afa24c
mikamo3 <kamo3proj@gmail.com>
2020-12-21 02:32:54 +0900

    fix

--- kawazu.fish
+++ kawazu.fish
@@ -1,22 +1,20 @@
 #!/usr/bin/env fish
-set fish_trace 1
 function kawazu
   set kawazu_root_dir
   set kawazu_dotfiles_dir
-  if [ "{$KAWAZU_ROOT_DIR}" = "" ]
-    set kawazu_root_dir {$HOME}/.kawazu/repos
+  if [ $KAWAZU_ROOT_DIR = "" ]
+    set kawazu_root_dir $HOME/.kawazu/repos
     else
     set kawazu_root_dir {$KAWAZU_ROOT_DIR}
   end
-  if [ "{$KAWAZU_DOTFILES_DIR}" = "" ]
-    set kawazu_dotfiles_dir {$HOME}/.kawazu/dotfiles
+  if [ "$KAWAZU_DOTFILES_DIR" = "" ]
+    set kawazu_dotfiles_dir $HOME/.kawazu/dotfiles
     else
-    set kawazu_dotfiles_dir {$KAWAZU_DOTFILES_DIR}
+    set kawazu_dotfiles_dir $KAWAZU_DOTFILES_DIR
   end
   if [ "$argv[1]" = "cd" ]
-    cd {$kawazu_dotfiles_dir}
+    cd $kawazu_dotfiles_dir
   else
-    {$kawazu_root_dir}/bin/kawazu $argv
+    $kawazu_root_dir/bin/kawazu $argv
   end
 end
-kawazu cd
